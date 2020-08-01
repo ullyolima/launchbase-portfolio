a61254bc840230abd980881783818495b53ca089
ullyolima <ullyolima@gmail.com>
2020-08-01 15:41:19 -0300

    Completed project

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const modalOverlay = document.querySelector(".modal-overlay")
 
 for (let card of cards) {
     card.addEventListener("click", function(){
+        const videoId = card.getAttribute("id")
         modalOverlay.classList.add("modal-overlay--active")
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
     })
 }
 
 closeModal.addEventListener("click", function(){
     modalOverlay.classList.remove("modal-overlay--active")
+    modalOverlay.querySelector("iframe").src = ""
 })
